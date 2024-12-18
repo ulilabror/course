98b98cf644d90864aeb58dc87b8b8ead8a6b9f75
ulilabror <root087732168347@gmail.com>
2024-12-18 17:57:36 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,12 +41,7 @@
                </button>
                <div class="collapse navbar-collapse" id="navbarSupportedContent">
                   <ul class="navbar-nav mr-auto">
-                     <li class="nav-item active">
-                        <a class="nav-link" href="index.html">Home</a>
-                     </li>
-                     <li class="nav-item">
-                        <a class="nav-link" href="contact.html">Contact Us</a>
-                     </li>
+                     
                   </ul>
                   <form class="form-inline my-2 my-lg-0">
                      <div class="login_text"><a href="admin/login">Login</a></div>
